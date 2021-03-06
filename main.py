e08e4859520b912dc05a188f1bb839560de86249
ojitos369 <ojitos369@gmail.com>
2021-03-05 23:03:52 -0600

    arreglo en mostrar base 10 en predefinidos

--- main.py
+++ main.py
@@ -16,22 +16,19 @@ def main():
                 pausar()
             elif opc == 2:
                 n = menus.numero('01.',2)
-                conversion(n,2,[8,16])
+                conversion(n,2,[10,8,16])
                 pausar()
             elif opc == 3:
                 n = menus.numero('012345678.',8)
-                conversion(n,8,[2,16])
+                conversion(n,8,[10,2,16])
                 pausar()
             elif opc == 4:
                 n = menus.numero('ABCDEF0123456789.',16)
-                conversion(n,16,[2,8])
+                conversion(n,16,[10,2,8])
                 pausar()
             elif opc == 5:
                 n,base,conversiones = menus.general()
-                if int(base) == 10:
-                    decimal(n,conversiones)
-                else: 
-                    conversion(n,base,conversiones)
+                conversion(n,base,conversiones)
                 pausar()
         else:
             print('Adios')
